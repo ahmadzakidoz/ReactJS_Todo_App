f250696e51514411e9916ea79c917f3dc87df3ae
ahmadzakidoz <ahmadzaki.doz@gmail.com>
2019-11-24 20:03:47 +0700

    Updates

---
+++ precache-manifest.0916774310691f9228d5e127a0a6d978.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99091008322bfdcb2930a7f5003f2dac",
+    "url": "/ReactJS_Todo_App/index.html"
+  },
+  {
+    "revision": "91947b429cbd41dfddb8",
+    "url": "/ReactJS_Todo_App/static/css/main.0522bb10.chunk.css"
+  },
+  {
+    "revision": "a142b755ff1f0b5fb84e",
+    "url": "/ReactJS_Todo_App/static/js/2.aa7c06f9.chunk.js"
+  },
+  {
+    "revision": "91947b429cbd41dfddb8",
+    "url": "/ReactJS_Todo_App/static/js/main.83a6177e.chunk.js"
+  },
+  {
+    "revision": "f9d981d25f76ecf967d2",
+    "url": "/ReactJS_Todo_App/static/js/runtime-main.54e5de9f.js"
+  }
+]);
